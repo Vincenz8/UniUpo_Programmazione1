780ca35c74feea34b09abbe99f8db846a3188992
Vincenzo <vincenzo.russ8@outlook.com>
2022-09-28 17:25:35 +0200

    primo esercizio

--- 1_variabili/1_variabili4.c
+++ 1_variabili/1_variabili4.c
@@ -2,5 +2,17 @@
 
 void main(){
 
-    
+    float x, y, h, area_trapezio;
+
+    printf("Inserisci base1: ");
+    scanf("%f", &x);
+    printf("Inserisci base2: ");
+    scanf("%f", &y);
+    printf("Inserisci Altezza: ");
+    scanf("%f", &h);
+
+    area_trapezio = ((x + y) * h) / 2;
+
+    printf("Area = %f\n", area_trapezio);
+
 }
