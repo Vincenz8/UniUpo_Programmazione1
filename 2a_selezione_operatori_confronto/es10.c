3aec102f0df117a481d3a1768653de1c06f59eac
Vincenzo <vincenzo.russ8@outlook.com>
2022-10-05 20:41:34 +0200

    es 10 e 11 consgena 2

---
+++ 2a_selezione_operatori_confronto/es10.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <math.h>
+
+void main(){
+
+    float a, b, c;
+    float delta;
+    float x;
+
+    printf("Inserisci coefficiente a: ");
+    scanf("%f", &a);
+    printf("Inserisci coefficiente b: ");
+    scanf("%f", &b);
+    printf("Inserisci coefficiente c: ");
+    scanf("%f", &c);
+
+    delta = pow(b, 2) - (4 * a * c); //formula discriminante 
+    printf("Delta: %f\n", delta);
+
+    if (delta > 0){
+
+        float x2;
+        x = (-b + sqrt(delta)) / (2 * a);
+        x2 = (-b - sqrt(delta)) / (2 * a);
+        printf("x1 = %.3f\tx2 = %.3f\n", x, x2);
+
+    }else if (delta == 0){
+
+        x = -b / (2 * a);
+        printf("x = %f\n", x);
+
+    }else
+        printf("Nessuna soluzione\n");
+
+}
---
+++ 2a_selezione_operatori_confronto/es11.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+void main(){
+
+    int prelievo;
+    int erogazione;
+
+    printf("Somma da prelevare: ");
+    scanf("%d", &prelievo);
+
+    erogazione = 0;
+    if (prelievo >=20){
+
+        if (prelievo >= 50){
+
+            int b_50 = prelievo / 50;
+            prelievo = prelievo % 50;
+            erogazione += 50 * b_50;
+            printf("Banconote da 50: %d ", b_50); 
+        }
+
+        if (prelievo >= 20){
+            int b_20 = prelievo / 20;
+            prelievo = prelievo % 20; 
+            erogazione += 20 * b_20;
+            printf("Banconote da 20: %d ", b_20);
+        }
+        printf("Somma erogata: %d\n", erogazione);
+
+    }else
+        printf("Impossibile erogare somma\n");
+
+    
+
+}
