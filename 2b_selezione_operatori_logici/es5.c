37e0256a681c8226279eaca88ef0e85c28c80c8b
Vincenzo <vincenzo.russ8@outlook.com>
2022-10-07 10:26:50 +0200

    es5 consegna 3, variabili logiche

--- 2b_selezione_operatori_logici/es5.c
+++ 2b_selezione_operatori_logici/es5.c
@@ -3,6 +3,7 @@
 void main(){
 
     int x, y;
+    int y_pos, x_0, y_0, x_neg;
 
     printf("Inserisci x: ");
     scanf("%d", &x);
@@ -11,32 +12,37 @@ void main(){
 
     printf("Coordinate (%d, %d)\n", x, y);
 
-    if (x == 0 && y == 0)
+    y_pos = (y > 0);
+    x_0 = (x == 0);
+    y_0 = (y == 0);
+    x_neg = (x < 0);
+
+    if (x_0 && y_0)
 
         printf("Il punto si trova sull'origine\n");
 
-    else if (x == 0)
+    else if (x_0)
 
         printf("Il punto si trova sull'asse y\n");
 
-    else if (y == 0)
+    else if (y_0)
 
         printf("Il punto si trova sull'asse x\n");
     
-    else if (x > 0 && y > 0)
+    else if (x > 0 && y_pos)
 
         printf("Il punto si trova nel primo quadrante\n");
 
-    else if (x < 0 && y > 0)
+    else if (x_neg && y_pos)
 
         printf("Il punto si trova nel secondo quadrante\n");
 
-    else if (x < 0 && y < 0)
+    else if (x_neg && y < 0)
 
         printf("Il punto si trova nel terzo quadrante\n");
 
     else 
 
-        printf("Il punto si trova nel quarto quadrante");
+        printf("Il punto si trova nel quarto quadrante\n");
 
 }
