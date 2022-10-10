4c4a0a2da33f6e60cb750b7991ce750edaf37369
Vincenzo <vincenzo.russ8@outlook.com>
2022-10-10 22:13:03 +0200

    es 1 e 2 Consegna 3

---
+++ 3_iterazione/es1.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+void main(){
+    int n_numeri;
+    int n, n_max;
+    int i;
+
+    printf("Quanti numeri vuoi inserire? ");
+    scanf("%d", &n_numeri);
+    n_max = 0;
+
+    for(i = 0; i < n_numeri; i++){
+        
+        printf("Inserisci n%d: ", i + 1);
+        scanf("%d", &n);
+
+        n_max = (n > n_max) ? n : n_max;
+
+    }
+    printf("Il maggiore e' %d\n", n_max);
+
+}
---
+++ 3_iterazione/es2.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+void main(){
+
+    int n_numeri;
+    int n;
+    int n_max1, n_max2;
+    int i;
+
+    printf("Quanti numeri vuoi inserire? ");
+    scanf("%d", &n_numeri);
+
+    n_max1 = 0;
+    n_max2 = 0;
+
+    for(i = 0; i < n_numeri; i++){
+
+        printf("Inserisci n%d: ", i+1);
+        scanf("%d", &n);
+
+        if (n > n_max1){
+
+            n_max2 = n_max1;
+            n_max1 = n;
+
+        } 
+        else if (n > n_max2)
+
+            n_max2 = n;
+            
+    }
+
+    printf("1)%d\t2)%d\n", n_max1, n_max2);
+}
