23540d9d74765f73150cbaf21d9336ac87b90cc7
Vincenzo <vincenzo.russ8@outlook.com>
2022-10-12 10:31:57 +0200

    esercizi consegna 3

---
+++ 3_iterazione/es18.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+void main(){
+    
+    int n;
+    int i, j;
+
+    printf("Inserisci n:");
+    scanf("%d", &n);
+
+    for(i = 0; i < n; i++){
+
+        for(j = 0; j < n; j++){
+
+            if(i == j)
+                printf("0 ");
+            else if (j > i)
+                printf("X ");
+            else
+                printf("Y ");
+
+        }
+        
+        printf("\n");
+    }
+
+    
+
+}
